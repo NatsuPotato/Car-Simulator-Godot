6c5c94e2c4a8bf68f580ea4c9b5ce89f48089abf
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-02-27 22:56:57 -0500

    Implement grounded detection

--- car.gd
+++ car.gd
@@ -7,31 +7,38 @@ const SIDE_TRACTION = 8
 
 # should probably remember to use delta in calculations
 func _physics_process(delta: float) -> void:
+	
+	# check if grounded
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(position, position + Vector3(0, -0.5, 0))
+	var result = space_state.intersect_ray(query)
+	
+	if (not result.is_empty()):
 		
-	var local_velocity = linear_velocity * transform.basis.get_rotation_quaternion()
-	
-	var forward_wheel_velocity = local_velocity.z
-	var side_wheel_velocity = local_velocity.x
-	
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	# wheel coercion: when grounded, side wheel velocity fizzles out to make us go straight
-	side_wheel_velocity = move_toward(side_wheel_velocity, 0, delta * SIDE_TRACTION)
-	
-	# wheel propulsion: when grounded and throttling, the car's velocity will increase in the forward direction
-	forward_wheel_velocity = move_toward(forward_wheel_velocity, input_dir.y * MAX_SPEED, delta * FORWARD_TRACTION)
-	
-	if (forward_wheel_velocity > MAX_SPEED):
-		forward_wheel_velocity = MAX_SPEED
-	
-	# apply velocity
-	var translational_velocity = Vector3(side_wheel_velocity, 0, forward_wheel_velocity) * transform.basis.get_rotation_quaternion().inverse()
-	
-	linear_velocity.x = translational_velocity.x
-	linear_velocity.z = translational_velocity.z
-	
-	# when you turn, the car turns at a rate proportional to the magnitude of the move vector
-	rotation.y -= input_dir.x * linear_velocity.length() * TURN_SPEED * delta
+		var local_velocity = linear_velocity * transform.basis.get_rotation_quaternion()
+		
+		var forward_wheel_velocity = local_velocity.z
+		var side_wheel_velocity = local_velocity.x
+		
+		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		
+		# wheel coercion: when grounded, side wheel velocity fizzles out to make us go straight
+		side_wheel_velocity = move_toward(side_wheel_velocity, 0, delta * SIDE_TRACTION)
+		
+		# wheel propulsion: when grounded and throttling, the car's velocity will increase in the forward direction
+		forward_wheel_velocity = move_toward(forward_wheel_velocity, input_dir.y * MAX_SPEED, delta * FORWARD_TRACTION)
+		
+		if (forward_wheel_velocity > MAX_SPEED):
+			forward_wheel_velocity = MAX_SPEED
+		
+		# apply velocity
+		var translational_velocity = Vector3(side_wheel_velocity, 0, forward_wheel_velocity) * transform.basis.get_rotation_quaternion().inverse()
+		
+		linear_velocity.x = translational_velocity.x
+		linear_velocity.z = translational_velocity.z
+		
+		# when you turn, the car turns at a rate proportional to the magnitude of the move vector
+		rotation.y -= input_dir.x * linear_velocity.length() * TURN_SPEED * delta
 	
 	
 	
