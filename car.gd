dcdf305fb8f93a39b64f0ff4ecf2735e35c39864
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-02-28 20:58:31 -0500

    Update friction so it's only 0 when wheels are grounded, add skybox

--- car.gd
+++ car.gd
@@ -13,10 +13,6 @@ func _ready() -> void:
 	grounded_vector_start = grounded_vectors[0]
 	grounded_vector_end = grounded_vectors[1]
 
-# TODO
-# visible wheel turning
-# turning system (dependent on move speed) is pretty bad
-
 # As good practice, you should replace UI actions with custom gameplay actions.
 # Input.is_action_just_pressed("ui_accept")
 # move_toward(a, b, lerp)
@@ -24,14 +20,19 @@ func _ready() -> void:
 # also rigidbodies prefer using forces over directly modifying velocities
 func _physics_process(delta: float) -> void:
 	
-	# check if grounded
+	var basis_rotation := transform.basis.get_rotation_quaternion()
+	
+	# check if wheels are grounded
 	var space_state := get_world_3d().direct_space_state
-	var query := PhysicsRayQueryParameters3D.create(position + grounded_vector_start, position + grounded_vector_end)
+	var query := PhysicsRayQueryParameters3D.create(position + grounded_vector_start * basis_rotation, position + grounded_vector_end * basis_rotation)
 	var result := space_state.intersect_ray(query)
 	
-	if (not result.is_empty()):
-		
-		var basis_rotation := transform.basis.get_rotation_quaternion()
+	if (result.is_empty()):
+		# if wheels aren't on the ground, have normal friction
+		physics_material_override.friction = 1
+	else:
+		# if wheels are on the ground, we will handle friction ourselves
+		physics_material_override.friction = 0
 		
 		var local_velocity := linear_velocity * basis_rotation
 		
@@ -40,13 +41,12 @@ func _physics_process(delta: float) -> void:
 		
 		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		
-		# wheel coercion:
-		# the car's lateral movement always fissles out
+		# wheel coercion: the car's lateral movement always fissles out
 		apply_central_force(Vector3(-lateral_wheel_velocity * LATERAL_TRACTION, 0, 0) * basis_rotation.inverse())
 		
-		print(forward_wheel_velocity)
-		# wheel propulsion:
-		# the car's forward movement is dependent on if/how you're throttling
+		# wheel propulsion: the car's forward acceleration is dependent on
+		# 1) if you're throttling
+		# 2) how fast you're already going forward
 		apply_central_force(Vector3(0, 0, input_dir.y * FORWARD_TRACTION * (1 - abs(forward_wheel_velocity) / MAX_SPEED)) * basis_rotation.inverse())
 		
 		# when you turn, the car turns at a rate proportional to the magnitude of the move vector
