2d273801cc03c0927e0249bdc472d58770248328
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-03-02 04:24:32 -0500

    Change turning to a torque rather than directly modifying rotation

--- car.gd
+++ car.gd
@@ -10,8 +10,11 @@ extends RigidBody3D
 @export var FORWARD_TRACTION : float = 6
 @export var LATERAL_TRACTION : float = 8
 
-@export var grounded_vector_start : Vector3
-@export var grounded_vector_end : Vector3
+@export var GROUNDED_RAY_START : Vector3
+@export var GROUNDED_RAY_END : Vector3
+
+# I kinda wanna overhaul everything to be wheel based
+# both front and back wheels apply a lateral drag force, countering spin and drift
 
 func _physics_process(delta: float) -> void:
 	
@@ -19,7 +22,7 @@ func _physics_process(delta: float) -> void:
 	
 	# check if wheels are grounded
 	var space_state := get_world_3d().direct_space_state
-	var query := PhysicsRayQueryParameters3D.create(position + grounded_vector_start * basis_rotation, position + grounded_vector_end * basis_rotation)
+	var query := PhysicsRayQueryParameters3D.create(position + GROUNDED_RAY_START * basis_rotation, position + GROUNDED_RAY_END * basis_rotation)
 	var result := space_state.intersect_ray(query)
 	
 	if (result.is_empty()):
@@ -44,6 +47,10 @@ func _physics_process(delta: float) -> void:
 		# 2) how fast you're already going forward
 		apply_central_force(Vector3(0, 0, input_dir.y * FORWARD_TRACTION * (1 - abs(forward_wheel_velocity) / MAX_SPEED)) * basis_rotation.inverse())
 		
-		# TODO should use a rotational force
-		# when you turn, the car turns at a rate proportional to the magnitude of the move vector
-		rotation.y += input_dir.x * forward_wheel_velocity * TURN_SPEED * delta
+		# when you turn, the car turns at a rate proportional to forward speed
+		# and inversely proportional to rotational speed
+		# (also when not turning, apply a "rotational drag" torque opposite of rotation)
+		if (abs(input_dir.x) > 0.01):
+			apply_torque(Vector3(0, input_dir.x * forward_wheel_velocity * TURN_SPEED / (1 + abs(angular_velocity.y)), 0))
+		else:
+			apply_torque(Vector3(0, -angular_velocity.y * abs(forward_wheel_velocity) * TURN_SPEED, 0))
